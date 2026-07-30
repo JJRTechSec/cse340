0273b621df5e14c9c26368818decdc46094d7c80
JJRTechSec <jjrtechsec@gmail.com>
2026-07-30 02:22:20 +0200

    - completed activities 1 & 2 for week04 (form submissions and sessions and flash messages)

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,10 @@
 import express from 'express';
-
 import { displayHome } from './controllers/index.js';
-import { organizationsPage } from './controllers/organizations.js';
+import { organizationsPage, showOrganizationDetailsPage } from './controllers/organizations.js';
 import { displayProjects, showProjectDetailsPage } from './controllers/projects.js';
 import { displayCategories, showCategoryDetailsPage, } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
-import { showOrganizationDetailsPage } from './controllers/organizations.js';
+import { showNewOrganizationForm, processNewOrganizationForm } from './controllers/new-organization.js';
 
 const router = express.Router();
 
@@ -17,6 +16,8 @@ router.get('/categories', displayCategories); // Categories page route
 router.get('/category/:id', showCategoryDetailsPage); // Route for category details page
 router.get('/project/:id', showProjectDetailsPage()); // Route for project details page
 router.get('/organization/:id', showOrganizationDetailsPage, showProjectDetailsPage());// Route for organization details page
+router.get('/new-organization', showNewOrganizationForm); // Route for new organization form
+router.post('/new-organization', processNewOrganizationForm); // Route to handle new organization form submission
 
 // error handling routes
 router.get('/test-error', testErrorPage); // Route to test 500 error handling
